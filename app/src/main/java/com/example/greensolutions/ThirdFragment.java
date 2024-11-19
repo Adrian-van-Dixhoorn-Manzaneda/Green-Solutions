10d2c9a567c31a8bacc4686f281df3c87cff26ec
Josue Bellota <josuebellota7@gmail.com>
2024-11-19 21:21:24 +0100

    correciones y primera version de home

--- app/src/main/java/com/example/greensolutions/ThirdFragment.java
+++ app/src/main/java/com/example/greensolutions/ThirdFragment.java
@@ -9,10 +9,10 @@ import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RutasFragment#newInstance} factory method to
+ * Use the {@link ThirdFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RutasFragment extends Fragment {
+public class ThirdFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,7 +23,7 @@ public class RutasFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public RutasFragment() {
+    public ThirdFragment() {
         // Required empty public constructor
     }
 
@@ -33,11 +33,11 @@ public class RutasFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RutasFragment.
+     * @return A new instance of fragment ThirdFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RutasFragment newInstance(String param1, String param2) {
-        RutasFragment fragment = new RutasFragment();
+    public static ThirdFragment newInstance(String param1, String param2) {
+        ThirdFragment fragment = new ThirdFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,6 +58,6 @@ public class RutasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rutas, container, false);
+        return inflater.inflate(R.layout.fragment_third, container, false);
     }
 }
